55695d536504ab988445b1d0e06ea80816085d87
SamirIbrahimzade <samir.ibrahimzade@ug.bilkent.edu.tr>
2019-02-10 16:36:51 +0300

    new version of groups fragment.(with dialog)

--- app/src/main/java/com/groupr/groupr/MainPage.java
+++ app/src/main/java/com/groupr/groupr/MainPage.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -20,27 +21,32 @@ public class MainPage extends AppCompatActivity {
         bottomNav.setOnNavigationItemSelectedListener(navListener);
         // starting from one of the fragments
 
-        // getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new PostFragment()).commit();
+         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new NewsFragment()).commit();
 
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+                Fragment oldFragment = new NewsFragment();
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
+                    Fragment selectedFragment = new NewsFragment();
 
                     switch (item.getItemId()){
                         case R.id.nav_addPost:
                             selectedFragment = new PostFragment();
                             break;
                         case R.id.nav_groups:
-                            selectedFragment = new GroupFragment();
+                            GroupDialog dialog = new GroupDialog();
+                            dialog.show(getSupportFragmentManager(),"GroupDialog");
+                            selectedFragment = oldFragment;
                             break;
                         case R.id.nav_profile:
                             selectedFragment = new ProfileFragment();
                             break;
                     }
+                    oldFragment = selectedFragment;
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
 
                     return true;
